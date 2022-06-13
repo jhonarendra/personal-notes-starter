68fed1c16f942dbdebe53cc1d5196d75ec376e4a
jhonarendra <jhonarendra@gmail.com>
2022-06-13 10:55:53 +0800

    add: handle limit title

--- src/components/note/NoteForm.jsx
+++ src/components/note/NoteForm.jsx
@@ -4,8 +4,24 @@ class NoteForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      count: 0
+      form: {
+        title: '',
+        body: ''
+      },
+      titleLimit: 50
     }
+  }
+
+  handleChange = (e) => {
+    const form = {...this.state.form}
+    if (e.target.name === 'title' && e.target.value.length > 50) {
+      return false
+    }
+    form[e.target.name] = e.target.value
+    this.setState({
+      form,
+      titleLimit: 50 - form.title.length
+    })
   }
   render() {
     return (
@@ -13,10 +29,26 @@ class NoteForm extends React.Component {
         <h2>Buat catatan</h2>
         <form>
           <p className="note-input__title__char-limit">
-            Sisa karakter: 50
+            Sisa karakter: { this.state.titleLimit }
           </p>
-          <input className="note-input__title" type="text" placeholder="Ini adalah judul ..." required="" value=""/>
-          <textarea className="note-input__body" type="text" placeholder="Tuliskan catatanmu di sini ..." required="">
+          <input
+            className="note-input__title"
+            type="text"
+            placeholder="Ini adalah judul ..."
+            required=""
+            value={this.state.form.title}
+            name="title"
+            onChange={this.handleChange}
+          />
+          <textarea
+            className="note-input__body"
+            type="text"
+            name="body"
+            placeholder="Tuliskan catatanmu di sini ..."
+            required=""
+            value={this.state.form.body}
+            onChange={this.handleChange}
+          >
           </textarea>
           <button type="submit">Buat</button>
         </form>
