4594216c8485ded5f32a81cb1943ad165f27fa28
jhonarendra <jhonarendra@gmail.com>
2022-06-13 11:42:02 +0800

    fix: style header

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -15,15 +15,17 @@ class HeaderSection extends React.Component {
   }
   render() {
     return (
-      <div className='note-app__header'>
-        <h1>Notes</h1>
-        <div className="note-search">
-          <input
-            type="text"
-            placeholder="Cari catatan ..."
-            value={this.state.search}
-            onChange={this.handleChange}
-          />
+      <div className='note-app__header-wrapper'>
+        <div className='note-app__header'>
+          <h1>Notes</h1>
+          <div className="note-search">
+            <input
+              type="text"
+              placeholder="Cari catatan ..."
+              value={this.state.search}
+              onChange={this.handleChange}
+            />
+          </div>
         </div>
       </div>
     )
